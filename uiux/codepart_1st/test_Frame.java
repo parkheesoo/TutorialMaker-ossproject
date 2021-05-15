0eda39f1022428a15cd010011ebd97c5c6497651
kimjm9841 <kimjm9841@naver.com>
2021-05-15 19:13:35 +0900

    divide two panel codes

--- uiux/codepart_1st/test_Frame.java
+++ uiux/codepart_1st/test_Frame.java
@@ -1,16 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.GridLayout;
 
 public class test_Frame extends JFrame {
-    test_MainPanel mainPanel;
     public test_Frame(){
-        mainPanel = new test_MainPanel();
+    	Container c = getContentPane();
+		c.setLayout(new GridLayout(1, 2));
+    	
+        codePanel CodePanel = new codePanel();
+        commentPanel CommentPanel = new commentPanel();
 
         setTitle("Tutorial Maker");
-        add(mainPanel, BorderLayout.CENTER);
+        add(CommentPanel);
+        add(CodePanel);
 
         setVisible(true);
-        setSize(1050,800);
+        setSize(1000,700);
         setLocationRelativeTo(null);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
